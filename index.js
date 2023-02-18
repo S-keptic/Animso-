12df191ccfcb337621c2db8bb409f3bfea826da8
sekire <69573771+leviackermannn@users.noreply.github.com>
2023-02-18 22:02:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let oregairu = new Audio('songs/Yukitoki (osanime.com).mp3');
+let asp = new Audio('songs/asphyixa.mp3');
+let oregai = document.getElementById('oregai');
+let asph = document.getElementById('as');
+let gurenge = document.getElementById('gurenge');
+let gur = new Audio('songs/guren.mp3');
+let crying = document.getElementById('crying');
+let cry = new Audio('songs/crying.mp3');
+let sparkle = new Audio('songs/sparkle.mp3');
+let spark = document.getElementById('sparkle');
+let renai = document.getElementById('renai');
+let ren = new Audio('songs/renai.mp3');
+let inferno = document.getElementById('inferno');
+let parasyte = document.getElementById('parasyte');
+let name1 = document.getElementById('name');
+let inf = new Audio('songs/inferno.mp3');
+let par = new Audio('songs/parasyte.mp3');
+let aot = new Audio('songs/aot.mp3');
+
+oregai.onclick = ()=>{
+
+    oregairu.play();
+
+
+}
+
+asph.onclick = ()=>{
+    asp.play();
+
+
+}
+
+gurenge.onclick = ()=>{
+    gur.play();
+}
+
+crying.onclick = ()=>{
+ cry.play();    
+}
+
+
+spark.onclick = ()=>{
+    sparkle.play();}
+
+
+renai.onclick = ()=>{
+    ren.play();
+}
+
+
+inferno.onclick = ()=>{
+    inf.play();
+}
+
+
+parasyte.onclick = ()=>{
+par.play();
+
+}
+
+name1.onclick = ()=>{
+aot.play();
+}
+
+
+
+
